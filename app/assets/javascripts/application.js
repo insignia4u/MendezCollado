2f50cb87891da81b97426631c46823257e783b55
Rafael Alejandro Lizardo Celiz <lizardoceliz@gmail.com>
2013-04-12 16:42:09 -0300

    [page Empresa]
      - Added obit slider plugins
      - Renamed home page to empresa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require index
+//= require foundation.orbit
 //= require jquery-picture-min
 
 $(document).foundation();
 
 $(function(){
     $('figure.responsive').picture();
+});
+
+$(document).ready(function() {
+
+  $(document).foundation('orbit', {bullets: true, timer_speed: 4000});
+
+  // Override Slider Header
+  $(".orbit-next, .orbit-prev").css({display: "none"});
+  $(".orbit-timer").css({display: "none"});
+  $(".orbit-slide-number").remove();
+  $(".orbit-bullets li").each( function() {
+    $(this).text($(this).attr("data-orbit-slide-number"));
+  } );
 });
